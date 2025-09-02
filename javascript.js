eb577aabff1d4009a2fdabb49ff83556742e5097
anandb104 <210169445+anandb104@users.noreply.github.com>
2025-09-02 09:27:31 +0530

    Change the ui and fix errors
    The ui of the game was changed and errors related to game going over the 5 score was fixed.

--- javascript.js
+++ javascript.js
@@ -1,14 +1,17 @@
-let x=Math.random();
+// let x=Math.random();
 // console.log(x);
 
 function getComputerChoice(number)
 {
-if(number<=0.33)return "Rock";
+if(number<=0.33)return "rock";
 else if(number > 0.33 && number <= 0.66)return "Scissor";
-else return "Paper";
+else return "paper";
 }
 
 // console.log(getComputerChoice(x));
+const img1=document.querySelector(".r-images");
+const img2=document.querySelector(".question");
+
 
 const rock=document.querySelector("#one");
 const paper=document.querySelector("#two");
@@ -17,10 +20,16 @@ const scissor=document.querySelector("#three");
 const div1=document.querySelector(".compchoice");
 const div2=document.querySelector(".compscore");
 const div3=document.querySelector(".humscore");
-const div4=document.querySelector(".bhu");
+const div4=document.querySelector(".message");
+
+const button1 = document.createElement("button");
+button1.classList.add("new");
+button1.textContent="Play Again?"
+button1.addEventListener("click",()=>location.reload());
+const div5=document.querySelector(".playagain");
 
 
-let input;
+// let input;
 // function playGame(){
 // rock.addEventListener("click",()=>{
 //     let x=Math.random()
@@ -66,7 +75,7 @@ let input;
 //         }
 // });
 // }
-function getHumanChoice(){
+function getHumanChoice(input){
 return input;
 }
 
@@ -80,43 +89,43 @@ function playRound(humanChoice, computerChoice) {
     //    console.log(`Your score=${humanScore} and computer score=${computerScore}.`);//string cocatenation
     div2.textContent=`${computerScore}`;
     div3.textContent=`${humanScore}`;
-    //    return "draw";
+    div4.textContent="Draw!!Both of you chose the same option!!";
 }
 else if(humanChoice.toUpperCase()=="ROCK" && computerChoice.toUpperCase()=="PAPER"){//error when rock and paper
     computerScore+=1;
     div2.textContent=`${computerScore}`;
     div3.textContent=`${humanScore}`;
-    // return "You lose! Paper beats Rock";
+    div4.textContent="You lose! Paper beats Rock";
 }
 else if(humanChoice.toUpperCase()=="PAPER" && computerChoice.toUpperCase()=="SCISSOR"){
     computerScore+=1;
     div2.textContent=`${computerScore}`;
     div3.textContent=`${humanScore}`;
-    // return "You lose! Scissor beats Paper";
+    div4.textContent="You lose! Scissor beats Paper";
 }
 else if(humanChoice.toUpperCase()=="ROCK" && computerChoice.toUpperCase()=="SCISSOR"){
     humanScore+=1;
     div2.textContent=`${computerScore}`;
     div3.textContent=`${humanScore}`;
-    // return "You Win! Rock beats Scissor";
+    div4.textContent="You Win! Rock beats Scissor";
 }
 else if(humanChoice.toUpperCase()=="SCISSOR" && computerChoice.toUpperCase()=="PAPER"){
     humanScore+=1;
     div2.textContent=`${computerScore}`;
     div3.textContent=`${humanScore}`;
-    // return "You Win! Scissor beats Paper";
+    div4.textContent="You Win! Scissor beats Paper";
 }
 else if(humanChoice.toUpperCase()=="PAPER" && computerChoice.toUpperCase()=="ROCK"){
     humanScore+=1;
     div2.textContent=`${computerScore}`;
     div3.textContent=`${humanScore}`;
-    // return "You Win! Paper beats Rock";
+    div4.textContent="You Win! Paper beats Rock";
 }
 else if(humanChoice.toUpperCase()=="SCISSOR" && computerChoice.toUpperCase()=="ROCK"){
     computerScore+=1;
     div2.textContent=`${computerScore}`;
     div3.textContent=`${humanScore}`;
-    // return "You lose! Rock beats Scissor";
+    div4.textContent="You lose! Rock beats Scissor";
 }
  }
 
@@ -143,49 +152,84 @@ function disableButtons() {
     paper.disabled = true;
     scissor.disabled = true;
 }
-function playGame(){
+    function playGame(){
     rock.addEventListener("click",()=>{
-        let x=Math.random()
         input="Rock";
-        div1.textContent=`Computer's choice is:${getComputerChoice(x)}`;
-        playRound(getHumanChoice(),getComputerChoice(x));
-        if(humanScore==5) {
-            div4.textContent+="Congrats you are a Winner :) ";   
-            disableButtons();
+        let x=Math.random();
+        let computerSelection = getComputerChoice(x);
+        console.log(x);
+        let humanSelection = getHumanChoice(input);
+        console.log(input);
+        img1.innerHTML = "<img class='lh'src='./images/rocker.png'>";
+        if(computerSelection=="rock")
+            div1.innerHTML = `<img class='lh'src='./images/${computerSelection}er.png'>`;
+            else{
+                div1.innerHTML = `<img class='lh'src='./images/${computerSelection}.png'>`;
             }
-            else if(computerScore==5){
-                   div4.textContent+="Ha ha you are a loser :(";
+        playRound(humanSelection,computerSelection);
+        if(humanScore>=5) {
+            div4.textContent="Congrats you are the Winner :).Want to Play Again? ";   
+            disableButtons();
+            div5.appendChild(button1);
+            }
+            else if(computerScore>=5){
+                   div4.textContent="Sorry you Lost :(.Want to Play Again?";
                    disableButtons();
+                   div5.appendChild(button1);
             }
             
     });
-    paper.addEventListener("click",()=>{
-        let x=Math.random()
+    paper.addEventListener("click",()=>{ 
         input="Paper";
-        div1.textContent=`Computer's choice is:${getComputerChoice(x)}`;
-        playRound(getHumanChoice(),getComputerChoice(x));
-        if(humanScore==5) {
-            div4.textContent+="Congrats you are a Winner :)";   
+        let x=Math.random();
+        let computerSelection = getComputerChoice(x);
+        console.log(x);
+        let humanSelection = getHumanChoice(input);
+        console.log(input);
+        img1.innerHTML = "<img class='lh'src='./images/paper.png'>"
+        if(computerSelection=="rock")
+        div1.innerHTML = `<img class='lh'src='./images/${computerSelection}er.png'>`;
+        else{
+            div1.innerHTML = `<img class='lh'src='./images/${computerSelection}.png'>`;
+        }
+        playRound(humanSelection,computerSelection);
+        if(humanScore>=5) {
+            div4.textContent="Congrats you are the Winner :).Want to Play Again?";   
             disableButtons();
+            div5.appendChild(button1);
             }
-            else if(computerScore==5){
-                   div4.textContent+="Ha ha you are a loser :(";
+            else if(computerScore>=5){
+                   div4.textContent="Sorry you Lost :(.Want to Play Again?";
                    disableButtons();
+                   div5.appendChild(button1);
+                   
             }
     });
     scissor.addEventListener("click",()=>{
-        let x=Math.random()
         input="Scissor";
-        div1.textContent=`Computer's choice is:${getComputerChoice(x)}`;
-        playRound(getHumanChoice(),getComputerChoice(x));
-        if(humanScore==5) {
-            div4.textContent+="Congrats you are a Winner :)";   
-            disableButtons();
+        let x=Math.random();
+        let computerSelection = getComputerChoice(x);
+        console.log(x);
+        let humanSelection = getHumanChoice(input);
+        console.log(input);
+        img1.innerHTML = "<img class='lh'src='./images/scissor.png'>"
+        if(computerSelection=="rock")
+            div1.innerHTML = `<img class='lh'src='./images/${computerSelection}er.png'>`;
+            else{
+                div1.innerHTML = `<img class='lh'src='./images/${computerSelection}.png'>`;
             }
-            else if(computerScore==5){
-                   div4.textContent+="Ha ha you are a loser :(";
+        playRound(humanSelection,computerSelection);
+        if(humanScore>=5) {
+            div4.textContent="Congrats you are the Winner :).Want to Play Again?";   
+            disableButtons();
+            div5.appendChild(button1);
+            }
+            else if(computerScore>=5){
+                   div4.textContent="Sorry you Lost :(.Want to Play Again?";
                    disableButtons();
+                   div5.appendChild(button1);
             }
     });
-    }
-  playGame();
+}
+playGame();
+    
